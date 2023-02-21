06ccd76c798271b8745e58aa188bc5eea132f8ad
takumi.ootsuki <heitaku443@gmail.com>
2023-02-21 10:59:18 +0000

    add commons/navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
+    @include('commons.navbar')
     <div class="container mx-auto">
         @include('commons.error_messages')
         @yield('content')
